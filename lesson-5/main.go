28a1b9797356f38f100c3eaf2f29a1dd0215f0ce
Maksym Hrebeniuk <myrktt@gmail.com>
2022-06-15 16:03:08 +0300

    refactor: change types

--- lesson-5/main.go
+++ lesson-5/main.go
@@ -5,17 +5,17 @@ import (
 	"math"
 )
 
-const (
+var (
 	applePrice = 5.99
 	pearPrice  = 7.
 	money      = 23.
 )
 
 func main() {
-	apples := 9.
-	pears := 8.
-	spending := (apples * applePrice) + (pears * pearPrice)
-	fmt.Printf("Скільки грошей треба витратити, щоб купити %g яблук та %g груш? %g\n", apples, pears, spending)
+	apples := 9
+	pears := 8
+	spending := (float64(apples) * applePrice) + (float64(pears) * pearPrice)
+	fmt.Printf("Скільки грошей треба витратити, щоб купити %d яблук та %d груш? %g\n", apples, pears, spending)
 
 	pearsPurchaseLimit, _ := math.Modf(money / pearPrice)
 	fmt.Println("Скільки груш ми можемо купити?", pearsPurchaseLimit)
@@ -23,9 +23,9 @@ func main() {
 	applesPurchaseLimit, _ := math.Modf(money / applePrice)
 	fmt.Println("Скільки яблук ми можемо купити?", applesPurchaseLimit)
 
-	applesAssumedQuantity := 2.
-	pearsAssumedQuantity := 2.
-	assumedQuantityCharge := (applesAssumedQuantity * applePrice) + (pearsAssumedQuantity * pearPrice)
+	applesAssumedQuantity := 2
+	pearsAssumedQuantity := 2
+	assumedQuantityCharge := (float64(applesAssumedQuantity) * applePrice) + (float64(pearsAssumedQuantity) * pearPrice)
 	isPurchasePossible := assumedQuantityCharge <= money
-	fmt.Printf("Чи ми можемо купити %g груші та %g яблука? %t\n", pearsAssumedQuantity, applesAssumedQuantity, isPurchasePossible)
+	fmt.Printf("Чи ми можемо купити %d груші та %d яблука? %t\n", pearsAssumedQuantity, applesAssumedQuantity, isPurchasePossible)
 }
